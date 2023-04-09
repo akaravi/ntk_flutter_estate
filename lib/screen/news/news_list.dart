425d652a33cf4302d0cb99cf0d9234c8070b64ec
MJ.Dell <javadparishani@gmail.com>
2023-04-09 21:19:48 +0430

    Update news_list.dart

--- lib/screen/news/news_list.dart
+++ lib/screen/news/news_list.dart
@@ -2,8 +2,15 @@ import 'package:base/src/models/entity/news/news_content_model.dart';
 import 'package:flutter/material.dart';
 import 'package:base/src/index.dart';
 import 'package:ntk_flutter_estate/screen/list_with_filter.dart';
+import 'package:ntk_flutter_estate/screen/news/news_model_adapter.dart';
 
 class NewsListScreen extends BaseListWithFilterScreen<NewsContentModel> {
   NewsListScreen({Key? key})
-      : super(key: key, title: 'News', controller: NewsListController(adapterCreatorFunction: (context, m, index) => News,));
+      : super(
+            key: key,
+            title: 'News',
+            controller: NewsListController(
+              adapterCreatorFunction: (context, m, index) =>
+                  NewsModelAdapter(model: m),
+            ));
 }
