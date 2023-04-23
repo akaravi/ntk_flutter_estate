8f02f6c387092c331d0d5699f2a56649af31cb83
MJ.Dell <javadparishani@gmail.com>
2023-04-24 00:26:48 +0430

    check import

--- lib/screen/add/sub_new_estate_5.dart
+++ lib/screen/add/sub_new_estate_5.dart
@@ -4,11 +4,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:ntk_flutter_estate/global_data.dart';
-import 'package:ntk_flutter_estate/screen/add/new_estate_screen.dart';
 import 'package:ntk_flutter_estate/screen/add/sub_new_estate_1.dart';
-
+import 'package:file_picker/file_picker.dart';
 import '../../controller/new_estate_controller.dart';
-
+// import 'package:base/src/backend/api/file/dio_uploader.dart';
 class SubNewEstate5 extends SubNewEstateBase {
   SubNewEstate5({Key? key, required NewEstateController controller})
       : super(key: key, controller: controller);
@@ -30,7 +29,7 @@ class _Container1State extends State<SubNewEstate5> {
               title: GlobalString.mainPic,
               widget: Container(
                   color: GlobalColor.colorAccent.withOpacity(.9).withAlpha(50),
-                  child: InkWell(onTap: () => {},
+                  child: InkWell(onTap: getFile,
                     child: Padding(
                       padding: const EdgeInsets.all(40.0),
                       child: Icon(
@@ -46,5 +45,18 @@ class _Container1State extends State<SubNewEstate5> {
           ])
       ],
     );
+  }
+
+  void getFile()async {
+    List<PlatformFile> file = (await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+      onFileLoading: (FilePickerStatus status) => print(status),
+    ))!
+        .files;
+    // var upload =await ChunkedUploader()
+    //     .upload(filePlatform: file.elementAt(0) );
+    // // var upload = FileUploadService().upload(File(file.first.path??""));
+    // print(upload.toString());
   }
 }
